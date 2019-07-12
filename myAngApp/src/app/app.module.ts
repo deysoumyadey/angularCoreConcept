16fde2fa7462cc86869785a346d8ff22614d9d68
deysoumyadey <dey.soumyadey@gmail.com>
2019-07-11 21:21:02 -0400

    Shared Module and Interface

--- myAngApp/src/app/app.module.ts
+++ myAngApp/src/app/app.module.ts
@@ -5,17 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
 import { CustomersComponent } from './customers/customers.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CustomersModule
-  ],  
-  bootstrap: [
-    AppComponent
-  ]
+  declarations: [ AppComponent ],
+  imports: [ BrowserModule, CustomersModule, SharedModule ],  
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- myAngApp/src/app/customers/customers-list/customers-list.component.ts
+++ myAngApp/src/app/customers/customers-list/customers-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { UseExistingWebDriver } from 'protractor/built/driverProviders';
+import { ICustomer } from 'src/app/shared/interfaces';
 
 @Component({
     selector: 'app-customers-list',
     templateUrl: './customers-list.component.html'
 })
 export class CustomersListComponent implements OnInit{
-    filteredCustomers: any[] = [];
+    filteredCustomers: ICustomer[] = [];
     custoemrsOrderTotal: number;
     currencyCode: string = 'USD';
-    
+
     constructor() {}
 
     ngOnInit() {
